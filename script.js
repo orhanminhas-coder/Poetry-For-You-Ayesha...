7a9293f212bd58d7b819f981d1a1309a099f26bb
orhanminhas-coder <orhanminhas@gmail.com>
2026-03-13 23:09:35 +0500

    Refactor countdown timer and button click handlers

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 let num = 3;
-
 let counter = document.getElementById("count");
 
-let timer = setInterval(()=>{
-
-num--;
+let timer = setInterval(function(){
 
 counter.innerText = num;
 
-if(num==0){
+num--;
+
+if(num < 0){
 
 clearInterval(timer);
 
-document.getElementById("count").style.display="none";
+counter.style.display = "none";
 
-document.getElementById("openBtn").style.display="inline-block";
+document.getElementById("openBtn").style.display = "inline-block";
 
 }
 
 },1000);
 
 
-document.getElementById("openBtn").onclick=function(){
+document.getElementById("openBtn").onclick = function(){
 
-document.getElementById("countdownPage").style.display="none";
+document.getElementById("page1").style.display = "none";
 
-document.getElementById("secondPage").classList.remove("hidden");
+document.getElementById("page2").classList.remove("hidden");
 
 document.getElementById("music").play();
 
